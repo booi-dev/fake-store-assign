f471b2579d3b819acdc98459dd41f80994994e4b
booi-dev <booimangang@gmail.com>
2023-07-29 09:01:03 +0530

    Initial commit

---
+++ src/components/main/main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <main className="min-h-[60vh] bg-yellow-400">
+      <h1> Hi, store with nav </h1>
+    </main>
+  );
+};
+
+export default Main;
---
+++ src/components/nav/nav.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav className="flex items-center justify-center h-20 bg-indigo-50 text-slate-950">
+      <div>navbar</div>
+    </nav>
+  );
+};
+
+export default Nav;
